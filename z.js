a1a0f34cd496e96dc039eb829d90f3be22c0f176
lvdev <lov3r.dev@gmail.com>
2024-06-20 13:56:10 +0100

    weather values

--- z.js
+++ z.js
@@ -59,3 +59,8 @@ search.addEventListener('click', () => {
                 default:
                     image.src = '';
             }
+
+            temperature.innerHTML = `${parseInt(json.main.temp)}<span>°C</span>`;
+            description.innerHTML = `${json.weather[0].description}`;
+            humidity.innerHTML = `${json.main.humidity}%`;
+            wind.innerHTML = `${parseInt(json.wind.speed)}Km/h`;
